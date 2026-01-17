7c9bea984aba4d6f8603ae7a4813ddfe3c1e6370
Mathias <mathias.kronemann@gmx.at>
2026-01-17 10:22:22 +0100

    Example Component and reset Stylings

--- stock-app-pwa/src/App.vue
+++ stock-app-pwa/src/App.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+</script>
 <template>
   <div>
     <h1>Hackathon App</h1>
+    <HelloWorld message="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
--- stock-app-pwa/src/components/HelloWorld.vue
+++ stock-app-pwa/src/components/HelloWorld.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-
+const props = defineProps<{ message: string }>();
 </script>
 
 <template>
   <div>
+    <h1>{{ props.message }}</h1>
   </div>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
